b004b3c0ed6f9960aa844bef2cf0172cc8a9d616
DoDofo1 <69332743+DoDofo1@users.noreply.github.com>
2020-08-13 11:30:57 +0700

    add artifact101,artifact102

---
+++ artifact101.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {View,Text} from 'react-native';
+function App(){
+  return (
+    <View style={{
+      justifyContent:"center",
+      flex:1,
+      alignItems:"center"
+      }}>
+
+      <Text style={{color:"red",
+      fontSize:"20px",
+      fontWeight:"bold"}}>
+        I Love My Cat
+        </Text>
+    </View>
+  )
+}
+export default App ;
